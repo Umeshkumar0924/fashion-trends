f285f946b7606bf596a3d4df2573460bb648df30
Umeshkumar0924 <umeshkumargupta0924@gmail.com>
2024-10-26 12:25:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("shopButton").addEventListener("click", function() {
+    alert("Redirecting to the shop...");
+    window.location.href = "https://www.example.com"; // Replace with actual shop URL
+});
